95d3f5c2a9b9639c29c0354d764cf0c373c24331
TTVictoria <146879934+TTVictoria@users.noreply.github.com>
2024-03-10 20:03:36 +0200

    src2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -95,3 +95,65 @@ let galleryLightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailForm = document.querySelector('[name="email"]');
+const messageForm = document.querySelector('[name="message"]');
+
+function loadSavedData() {
+  const savedFeedback = localStorage.getItem('feedback-form-state');
+  if (savedFeedback) {
+    try {
+      const parsedFeedback = JSON.parse(savedFeedback);
+      emailForm.value = parsedFeedback.email;
+      messageForm.value = parsedFeedback.message;
+    } catch (error) {
+      console.error('Error parsing saved feedback:', error);
+    }
+  }
+}
+
+function validateFields() {
+  return emailForm.value.trim() !== '' && messageForm.value.trim() !== '';
+}
+
+function saveDataToLocalStorage() {
+  const email = emailForm.value.trim();
+  const message = messageForm.value.trim();
+
+  const dataInput = {
+    email: email,
+    message: message,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataInput));
+}
+
+feedbackForm.addEventListener('input', event => {
+  saveDataToLocalStorage();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadSavedData();
+});
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (validateFields()) {
+    const submittedEmail = emailForm.value.trim();
+    const submittedMessage = messageForm.value.trim();
+
+    console.log({
+      email: submittedEmail,
+      message: submittedMessage,
+    });
+
+    emailForm.value = '';
+    messageForm.value = '';
+
+    localStorage.removeItem('feedback-form-state');
+  } else {
+    console.error('Please fill in all fields.');
+  }
+});
